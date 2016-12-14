0f9201faa5bbfbea8cb8ada8e421f94a660d7a69
Max Seidman <max@tiltfactor.org>
2016-12-14 11:34:48 -0500

    Reenable and hardcode highscores screen
    The highscores on this screen will need to be updated to the
    current ones when we pull this to the server.
    
    Also added a "Highscores are no longer being recorded" message
    that may want to be swapped out.

--- src/Beanstalk/wwwroot/js/screens/highscores-screen.js
+++ src/Beanstalk/wwwroot/js/screens/highscores-screen.js
@@ -14,7 +14,7 @@ var HighscoresScreen = (function (_super) {
         var _this = this;
         _super.prototype.init.call(this);
         this.allTimeRows = $("#highscoresScreen .alltime .row");
-        this.weeklyRows = $("#highscoresScreen .weekly .row");
+        //this.weeklyRows = $("#highscoresScreen .weekly .row");
         // Listen for clicks
         $("#highscoresScreen button.back").click(function () { return beanstalk.screens.open(beanstalk.screens.main); });
         $("#highscoresScreen button.share").click(function () { return _this.share(); });
@@ -27,12 +27,13 @@ var HighscoresScreen = (function (_super) {
         _super.prototype.show.call(this);
         // Start off by updating the view
         this.setRowsToLoading(this.allTimeRows);
-        this.setRowsToLoading(this.weeklyRows);
-        var allTimeScoresPromise = beanstalk.backend.loadHighscores(3, new Date(0), new Date());
-        var weeklyTimeScoresPromise = beanstalk.backend.loadHighscores(3, moment().subtract(1, "week").toDate(), new Date());
+        //this.setRowsToLoading(this.weeklyRows);
+        //var allTimeScoresPromise = beanstalk.backend.loadHighscores(3, new Date(0), new Date());
+        //var weeklyTimeScoresPromise = beanstalk.backend.loadHighscores(3, moment().subtract(1, "week").toDate(), new Date());
         // HACK! This is because the TSD for parse is a bit crap
-        var parse = Parse;
-        if (beanstalk.user.backendBeanstalk == null) {
+        //var parse = Parse;
+        _this.setRow(_this.allTimeRows.get(3), "..", "..", "..");
+        /*if (beanstalk.user.backendBeanstalk == null) {
             parse.Promise.when(allTimeScoresPromise, weeklyTimeScoresPromise).then(function (allTime, weekly) {
                 _this.setRowsFromBeanstalks(_this.allTimeRows.slice(0, 3), allTime);
                 _this.setRowsFromBeanstalks(_this.weeklyRows.slice(0, 3), weekly);
@@ -49,17 +50,17 @@ var HighscoresScreen = (function (_super) {
                 _this.setPlayerRow(_this.allTimeRows.get(3), beanstalk.user.backendBeanstalk, alltimeRank + 1);
                 _this.setPlayerRow(_this.weeklyRows.get(3), beanstalk.user.backendBeanstalk, weeklyRank + 1);
             });
-        }
+        }*/
     };
     HighscoresScreen.prototype.setRowsToLoading = function (rows) {
-        rows.each(function (i, e) {
+        /*rows.each(function (i, e) {
             $(e).find(".position").text("");
             $(e).find(".name").text("loading..");
             $(e).find(".height").text("");
-        });
+        });*/
     };
     HighscoresScreen.prototype.setRowsFromBeanstalks = function (rows, beanstalks) {
-        rows.each(function (i, e) {
+        /*rows.each(function (i, e) {
             if (i < beanstalks.length) {
                 var name = Utils.getNameFromEmail(beanstalks[i].get("user").getUsername());
                 name = Utils.truncate(name, 13);
@@ -72,20 +73,20 @@ var HighscoresScreen = (function (_super) {
                 $(e).find(".name").text("..");
                 $(e).find(".height").text("..");
             }
-        });
+        });*/
     };
     HighscoresScreen.prototype.setPlayerRow = function (row, obj, rank) {
-        var name = Utils.getNameFromEmail(beanstalk.backend.user.getUsername());
+        /*var name = Utils.getNameFromEmail(beanstalk.backend.user.getUsername());
         name = Utils.truncate(name, 13);
         $(row).find(".position").text(Utils.getGetOrdinal(rank));
         $(row).find(".name").text(name);
-        $(row).find(".height").text(obj.get("height"));
+        $(row).find(".height").text(obj.get("height"));*/
     };
     HighscoresScreen.prototype.setRow = function (row, position, name, height) {
-        name = Utils.truncate(name, 13);
+       /* name = Utils.truncate(name, 13);
         $(row).find(".position").text(position);
         $(row).find(".name").text(name);
-        $(row).find(".height").text(height);
+        $(row).find(".height").text(height);*/
     };
     return HighscoresScreen;
 })(ScreenBase);
